89396c327fe1148feb63e2b70adf4a3486fc9be6
Preston TW <preston_tunnell_wilson@brown.edu>
2025-12-02 22:42:42 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            just
+            rustup
+            watchexec
+          ];
+        };
+      });
+}
+
